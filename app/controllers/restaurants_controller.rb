532eaf4c64743426a9e2e0db3a45b216822f6c32
Wyatt Andersen <wandersen02@gmail.com>
2018-06-04 00:05:17 -0500

    updates

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,5 +1,10 @@
 class RestaurantsController < ApplicationController
   before_action :authenticate_user!
+  before_action :authenticate_admin, only: [:edit, :update, :show]
+
+  def index
+    @restaurants = Restaurant.all
+  end
 
   def new
     @restaurant = Restaurant.new
@@ -11,12 +16,28 @@ class RestaurantsController < ApplicationController
     redirect_to new_order_path
   end
 
+  def edit
+    @restaurant = Restaurant.find_by(id: params[:id])
+  end
+
+  def update
+    @restaurant = Restaurant.find_by(id: params[:id])
+    @restaurant.update(restaurant_params)
+
+    redirect_to restaurant_path(id: @restaurant.id)
+  end
+
+  def show
+    @restaurant = Restaurant.find_by(id: params[:id])
+  end
+
   private
 
   def restaurant_params
     params.require(:restaurant).permit(
       :name,
-      :menu_url
+      :menu_url,
+      :website
     )
   end
 end
---
+++ db/migrate/20180604043640_add_website_to_restaurant.rb
@@ -0,0 +1,5 @@
+class AddWebsiteToRestaurant < ActiveRecord::Migration[5.0]
+  def change
+    add_column :restaurants, :website, :string
+  end
+end
