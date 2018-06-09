8cb846f9007cf34a3046fc3321c74d80f97d54b4
Wyatt Andersen <wandersen02@gmail.com>
2018-06-09 14:09:57 -0500

    Seeds

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180605020255) do
+ActiveRecord::Schema.define(version: 20180609180846) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,12 +40,13 @@ ActiveRecord::Schema.define(version: 20180605020255) do
   end
 
   create_table "restaurants", force: :cascade do |t|
-    t.string   "name",       null: false
+    t.string   "name",            null: false
     t.string   "menu_url"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
     t.string   "website"
     t.boolean  "approved"
+    t.integer  "creator_user_id"
   end
 
   create_table "users", force: :cascade do |t|
