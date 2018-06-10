ff99e16105087290015aac71f71d1c8177a6a5a9
Wyatt Andersen <wandersen02@gmail.com>
2018-06-10 10:13:26 -0500

    wip

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: "_ordervore_session_#{HEROKU_ENV}"
+Rails.application.config.session_store :cookie_store, key: "_ordervore_session_#{ENV.fetch('HEROKU_ENV')}"
