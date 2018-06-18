dda2db0e6fd7999f14d0f64d65b5c869ef38f31f
Wyatt Andersen <wandersen02@gmail.com>
2018-06-17 21:01:12 -0500

    autocomplete

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,9 +12,10 @@ class UsersController < ApplicationController
     @users = []
 
     if params[:search]
-      users_by_name = User.where("name LIKE ?", "%#{params[:search]}%")
-      users_by_email = User.where("email LIKE ?", "%#{params[:search]}%")
-      users_by_handle = User.where("handle LIKE ?", "%#{params[:search]}%")
+      search = params[:search].downcase
+      users_by_name = User.where('LOWER(name) LIKE ?', "%#{search}%")
+      users_by_email = User.where('LOWER(email) LIKE ?', "%#{search}%")
+      users_by_handle = User.where('LOWER(handle) LIKE ?', "%#{search}%")
       @users << users_by_name
       @users << users_by_email
       @users << users_by_handle
