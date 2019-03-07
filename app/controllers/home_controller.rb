23d30de937b42d0ef226a63103aee384893f16aa
Wyatt Andersen <wandersen02@gmail.com>
2019-03-06 19:58:29 -0600

    Updates

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,13 +4,20 @@ class HomeController < ApplicationController
 
   def support
   end
-  
+
   def contribute
     if ENV['ACCEPT_CONTRIBUTIONS'].nil?
       return redirect_to root_path
     end
   end
-  
+
   def feedback
+  end
+
+  def analytics
+    return redirect_to '/' unless current_user && current_user.admin?
+    @users = User.count
+    @orders = Order.count
+    @restaurants = Restaurant.count
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,9 +12,9 @@ class UsersController < ApplicationController
     @users = []
 
     if params[:search]
-      users_by_name = User.where("name LIKE ?", "%#{params[:search]}%")
-      users_by_email = User.where("email LIKE ?", "%#{params[:search]}%")
-      users_by_handle = User.where("handle LIKE ?", "%#{params[:search]}%")
+      users_by_name = User.where("name ILIKE ?", "%#{params[:search]}%")
+      users_by_email = User.where("email ILIKE ?", "%#{params[:search]}%")
+      users_by_handle = User.where("handle ILIKE ?", "%#{params[:search]}%")
       @users << users_by_name
       @users << users_by_email
       @users << users_by_handle
